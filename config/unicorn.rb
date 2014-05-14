70fef4098a9112904a13acf3c15c412628b0d617
tianlu1677 <tianlu1677.com>
2014-05-14 21:24:38 +0800

    add unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+root = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
+# Define worker directory for Unicorn
+working_directory root
+
+# Location of PID file
+pid "#{root}/tmp/pids/unicorn.pid"
+
+# Define Log paths
+stderr_path "#{root}/log/unicorn_err.log"
+stdout_path "#{root}/log/unicorn_out.log"
+
+# Listen on a UNIX data socket
+listen "/tmp/unicorn.sock"
+
+# 16 worker processes for production environment
+worker_processes 16
+
+# Load rails before forking workers for better worker spawn time
+preload_app true
+
+# Restart workes hangin' out for more than 240 secs
+timeout 240
